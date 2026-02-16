c16795b24b185b44393cacfd8321efa6c759717c
Your Name <your_email@example.com>
2026-02-16 16:58:44 +0200

    three added

--- src/compontent/header/Silder.jsx
+++ src/compontent/header/Silder.jsx
@@ -5,6 +5,9 @@ import { Link } from 'react-router-dom';
 // Import Swiper styles
 import 'swiper/css';
 import 'swiper/css/pagination';
+import SilderHeroOne from "../../img/banner_Hero1.jpg"
+import SilderHeroTwo from "../../img/banner_Hero2.jpg"
+import SilderHeroThree from "../../img/banner_Hero3.jpg"
 
 // import './styles.css';
 
@@ -29,7 +32,7 @@ function Silder() {
                                 <p>windows xp/10/7/8 ps3, tv ,Box</p>
                                 <Link to="/" className="btn">Shop Now</Link>
                             </div>
-                            <img src="../../img/banner_Hero1.jpg" alt="silder-hero 1" />
+                            <img src={SilderHeroOne} alt="silder-hero 1" />
                         </SwiperSlide>
                         <SwiperSlide>
                             <div className="content">
@@ -38,7 +41,7 @@ function Silder() {
                                 <p>windows xp/10/7/8 ps3, tv ,Box</p>
                                 <Link to="/" className="btn">Shop Now</Link>
                             </div>
-                            <img src="../../img/banner_Hero2.jpg" alt="silder-hero 1" />
+                            <img src={SilderHeroTwo} alt="silder-hero 1" />
                         </SwiperSlide>
                         <SwiperSlide>
                             <div className="content">
@@ -47,7 +50,7 @@ function Silder() {
                                 <p>windows xp/10/7/8 ps3, tv ,Box</p>
                                 <Link to="/" className="btn">Shop Now</Link>
                             </div>
-                            <img src="../../img/banner_Hero3.jpg" alt="silder-hero 1" />
+                            <img src={SilderHeroThree} alt="silder-hero 1" />
                         </SwiperSlide>
                     </Swiper>
                 </div>
